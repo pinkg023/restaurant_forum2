5899ad2d4c41040316fff22a62506199971f1ce6
pinkg023 <pinkg023@gmail.com>
2017-12-24 17:31:47 +0800

    setup restaurant resources for public users

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,9 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root "restaurants#index"        #首頁指向 RestaurantsController 的 index action
 
+  resources :restaurants, only: [:index, :show]
+  root "restaurants#index"
+
   namespace :admin do
     resources :restaurants
     resources :categories   # 加入Categories資料表相關用route
