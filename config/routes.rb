7b82cf5696a4d880b6404cd68b8de567fa4454b6
pinkg023 <pinkg023@gmail.com>
2018-01-04 09:44:41 +0800

    setup comment resources for public users

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,9 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root "restaurants#index"        #首頁指向 RestaurantsController 的 index action
 
-  resources :restaurants, only: [:index, :show]
+  resources :restaurants, only: [:index, :show] do
+    resources :comments, only: [:create, :destroy]
+  end
   root "restaurants#index"
 
   resources :categories, only: :show
