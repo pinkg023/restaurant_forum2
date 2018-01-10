1381c5119ba6ed2f2d8acc1b8a516570dd6eb975
pinkg023 <pinkg023@gmail.com>
2018-01-10 15:29:43 +0800

    finished user profile function

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,9 +1,38 @@
 class UsersController < ApplicationController
-  before_action :set_user, :only => [:show, :edit, :update]
+  before_action :set_user, :check_userself, :only => [:show, :edit, :update]
+
+  def index
+    @users = User.all
+    @restaurants = Restaurant.all
+    @categories = Category.all
+  end
+
+  def update
+
+      if @user.update(user_params)
+        flash[:notice] = "資料已更新！"
+        redirect_to restaurants_path      
+      else
+        render :edit
+        flash[:alert] = "資料更新失敗！"
+      end
+
+  end
 
   private
     def set_user
       @user = User.find(params[:id])
     end
-  end
+
+    def user_params
+      params.require(:user).permit(:email, :intro, :role, :image, :name)
+    end
+
+    def check_userself
+      if current_user!=@user
+      redirect_to restaurants_path
+      flash[:alert] = "您無編輯權限！"
+      end
+    end
+    
 end
