797597f4d81d3d43b03132165863b6405224caaa
pinkg023 <pinkg023@gmail.com>
2018-01-19 16:08:54 +0800

    setup routes for favorite / unfavorite

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -2,9 +2,8 @@ class Restaurant < ApplicationRecord
   validates_presence_of :name
   mount_uploader :image, PhotoUploader
   belongs_to :category
-  has_many :comments
 
-  has_many :comments, dependent: :restrict_with_error
+  has_many :comments, dependent: :restrict_with_error #comments存在時user無法被刪除
   has_many :users, through: :comments
 
   has_many :favorites, dependent: :destroy
--- config/routes.rb
+++ config/routes.rb
@@ -15,8 +15,10 @@ Rails.application.routes.draw do
     # 瀏覽個別餐廳的 Dashboard
     member do
       get :dashboard
+      post :favorite
+      post :unfavorite      
     end
-    
+
   end
   root "restaurants#index"
 
