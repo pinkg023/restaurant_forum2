284f5b01a900da5b937cce62989f62f5c0c3b4be
pinkg023 <pinkg023@gmail.com>
2018-01-19 15:07:40 +0800

    create_favorite_model&related_to_restaurants&users_table

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -4,4 +4,11 @@ class Restaurant < ApplicationRecord
   belongs_to :category
   has_many :comments
 
+  has_many :comments, dependent: :restrict_with_error
+  has_many :users, through: :comments
+
+  has_many :favorites, dependent: :destroy
+  has_many :favorited_users, through: :favorites, source: :user
+
+
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,8 +3,19 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
-  has_many :comments
   mount_uploader :image, PhotoUploader
+
+  # 「使用者評論很多餐廳」的多對多關聯
+  has_many :comments, dependent: :restrict_with_error #comments存在時user無法被刪除
+  has_many :restaurants, through: :comments
+
+  # 「使用者收藏很多餐廳」的多對多關聯
+  has_many :favorites, dependent: :destroy            #favorite存在時，刪除user會將favorite一併刪除
+  has_many :favorited_restaurants, through: :favorites, source: :restaurant
+
+  #上述兩組User<->Restaurant透過兩張表格關聯，故需要兩個方法名稱
+  #透過comments關聯的方法為User.restaurant
+  #而透過favorites關聯的方法為User.favorited_restaurants
 
   def admin?
     self.role == "admin"
