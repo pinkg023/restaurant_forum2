79c143d7fe3a8c210a9819a39d7a822d9d8f1271
pinkg023 <pinkg023@gmail.com>
2018-01-21 00:05:01 +0800

    add user index page with follow button & add create action for followship record

--- app/models/followship.rb
+++ app/models/followship.rb
@@ -1,4 +1,6 @@
 class Followship < ApplicationRecord
+  validates :following_id, uniqueness: { scope: :user_id }  #特定的 user_id 下，只能有一個 followings_id
+
   belongs_to :user
   belongs_to :following, class_name: "User"
 end
